84dc266de23ea2ffbdfc41f059eb2742c336e3ee
theDebbister <deborah@jakobis.net>
2023-08-10 17:58:50 +0200

    check nan values

--- scratchpad.py
+++ scratchpad.py
@@ -19,6 +19,17 @@ PATHS = [
     'eyetracking_data/scanpaths_reader_rm_wf/reader0_b0_merged_sp_rm.txt',
 ]
 
+PATHS_FOLDERS = [
+    # 'stimuli/aoi_texts/aoi/',
+    'stimuli/word_features/',
+    'stimuli/text_tags/',
+    # 'eyetracking_data/fixations/',
+    'eyetracking_data/reader_rm_wf/',
+    'eyetracking_data/reading_measures/',
+    'eyetracking_data/scanpaths/',
+    'eyetracking_data/scanpaths_reader_rm_wf/',
+]
+
 
 def check_column_names_type():
     with open('cols_and_data_type.txt', 'w', encoding='utf8') as f:
@@ -57,9 +68,39 @@ def check_word_text_cols():
                     f.write(f'Line: {idx + 2}\n')
                     f.write(f'Text: {text}\tWord: {word}\n\n')
 
-def
+
+def check_col_vals():
+    for folder in PATHS_FOLDERS:
+        cols = []
+
+        for path in Path(folder).iterdir():
+
+            try:
+                csv = pd.read_csv(path, sep=r'\s+', keep_default_na=False, na_values=[''])#, '#N/A', '#N/A N/A', '#NA', '-1.#IND', '-1.#QNAN', '-NaN', '-nan', '1.#IND', '1.#QNAN', '<NA>', 'N/A', 'NA', 'NaN', 'None', 'n/a', 'nan'])
+                # f.write('## delimiter = spaces\n\n')
+            except:
+                try:
+                    csv = pd.read_csv(path, sep='\t', keep_default_na=False, na_values=[''])#, '#N/A', '#N/A N/A', '#NA', '-1.#IND', '-1.#QNAN', '-NaN', '-nan', '1.#IND', '1.#QNAN', '<NA>', 'N/A', 'NA', 'NaN', 'None', 'n/a', 'nan'])
+                    # f.write('## delimiter = tab\n\n')
+                except:
+                    print(path)
+                    print('\n')
+                    continue
+
+            # cur_cols = [c for c in csv.columns]
+            # if cur_cols != cols:
+            #     print(path)
+            #     print(csv.columns)
+            #     cols = cur_cols
+
+            nans = csv.isna().sum().sum()
+            if nans != 0:
+                print(path)
+                print(f'Nans: {nans}\n\n')
+                print(csv.isna().sum())
 
 
 if __name__ == '__main__':
     # check_column_names_type()
-    check_word_text_cols()
+    # check_word_text_cols()
+    check_col_vals()
